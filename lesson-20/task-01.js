cbf9aed6b53bc5a0504145a82e76c561b7210bdc
Mashchenko Oleksandra <dremka@hotmail.com>
2019-06-10 00:40:38 +0300

    hw lesson-20

---
+++ lesson-20/task-01.js
@@ -0,0 +1,56 @@
+class Customers {
+    constructor() {
+        this._store = []
+    }
+    
+    add(entity) {
+        const properties = Object.keys(entity);
+        if(arguments.length !== 1) {
+            throw new Error('The method should be invoked with 1 parameter!')
+        } else if (typeof entity !== 'object') {
+            throw new Error ('The parameter should be an object!')
+        } else if (!entity.name) {
+            throw new Error ('Object does not valid: property "name" is required!')
+        } else if (properties.length === 2 && properties.some(item => item !== 'name' && item !== 'verified')) {
+            throw new Error('Object does not valid: property should be one of "name" or "verified"')
+        }
+
+
+        this._store.push(entity);
+    }
+
+    [ Symbol.iterator ]() {
+        let i = 0;
+        const iterableEntities = this._store.filter(entity => entity.hasOwnProperty('verified') && entity.verified === true);
+
+        return {
+            next: () => {
+                const done = i >= iterableEntities.length;
+                const value = !done ? iterableEntities[ i++ ] : undefined;
+
+                return {
+                    value,
+                    done
+                } 
+            },
+        };
+    }
+}
+
+const customers = new Customers();
+
+customers.add({name: 'Alex'});
+customers.add({name: 'Victor'});
+customers.add({name: 'Marcus'});
+customers.add({name: 'Andrii', verified: true});
+customers.add({name: 'Marco', verified: false});
+customers.add({name: 'Oliver'});
+customers.add({name: 'Lisa', verified: true});
+customers.add({name: 'John'});
+customers.add({name: 'Ivan', verified: true});
+
+console.log('customers', customers);
+
+for (const customer of customers) {
+    console.log(customer);
+}
